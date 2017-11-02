3868dbd88059a16ff91b05927059ce5448652c57
Neena van de Water <nkuriakose@hotmail.com>
2017-11-02 19:41:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  number = 10
+    while number < 1
+      number -=
+      puts "#{number} SECOND(S)!"
+    end  
+  puts "HAPPY NEW YEAR!"  
 end
